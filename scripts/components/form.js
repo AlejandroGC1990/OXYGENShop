c8b3d4342198b15e6c57d55c0f2dbb82ecd666cf
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-07 22:45:44 +0200

    validation form

--- scripts/components/form.js
+++ scripts/components/form.js
@@ -5,30 +5,45 @@ https://developer.mozilla.org/es/docs/Learn/Forms/Form_validation#la_api_de_vali
 
 const name = document.getElementById("name");
 const email = document.getElementById("email");
-const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const terms = document.getElementById("terms");
+
+const emailPattern =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 //validar input email
-email.addEventListener('input', (event) => {
-    if (email.value === '') {
-        email.setCustomValidity('The e-mail field cannot be empty.');
-    } else if (!emailPattern.test(email.value)) {
-        email.setCustomValidity('Please enter a valid e-mail address.');
-    } else {
-        email.setCustomValidity('');
-    }
-    email.reportValidity();
-})
+email.addEventListener("input", (event) => {
+  if (email.value === "") {
+    email.setCustomValidity("The e-mail field cannot be empty.");
+  } else if (!emailPattern.test(email.value)) {
+    email.setCustomValidity("Please enter a valid e-mail address.");
+  } else {
+    email.setCustomValidity("");
+  }
+  email.reportValidity();
+});
 
 //validar formulario
-const form = document.getElementById('contact_form');
-form.addEventListener('submit', (event) => {
-    if(!emailPattern.test(email.value)) {
-        email.classList.add('invalid');
-        email.setCustomValidity('Please enter a valid e-mail address.');
-        email.reportValidity();
-        email.preventDefault();
-    } else {
-        email.classListremove('invalid');
-        email.setCustomValidity('');
-    }
+const form = document.getElementById("contact_form");
+form.addEventListener("submit", (event) => {
+  if (!emailPattern.test(email.value)) {
+    email.classList.add("invalid");
+    email.setCustomValidity("Please enter a valid e-mail address.");
+    email.reportValidity();
+    email.preventDefault();
+  } else {
+    email.classListremove("invalid");
+    email.setCustomValidity("");
+  }
+
+  if (!terms.checked) {
+    terms.setCustomValidity("You must accept the terms and conditions.");
+    terms.reportValidity("");
+    isValid = false;
+  } else {
+    terms.setCustomValidity("");
+  }
+
+  if (!isValid) {
+    event.preventDefault();
+  }
 });
