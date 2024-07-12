eb73c5f1a9adb50d8d426d2b80d446db4de7a635
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-12 12:58:58 +0200

    corrections with the mentor

--- scripts/components/currentSelector.js
+++ scripts/components/currentSelector.js
@@ -1,74 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
+//definir símbolos con monedas
+const currencySymbols = {
+  eur: "€",
+  usd: "$",
+  gbp: "£",
+};
 
-  // Función para obtener los tipos de cambio desde la API
-  async function fetchExchangeRates(baseCurrency) {
-    const apiEndpoint = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`;
+// Función para obtener los tipos de cambio desde la API
+async function fetchExchangeRates(baseCurrency) {
+  const apiEndpoint = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`;
 
-    try {
-      const response = await fetch(apiEndpoint);
-      if (!response.ok) {
-        throw new Error("Failed to fetch exchange rates");
-      }
-      const data = await response.json();
-      console.log("Fetched Exchange Rates:", data);
-      return data.eur;
-    } catch (error) {
-      console.error("Error fetching exchange rates:", error.message);
-      return null;
+  try {
+    const response = await fetch(apiEndpoint);
+    if (!response.ok) {
+      throw new Error("Failed to fetch exchange rates");
     }
-  };
+    const data = await response.json();
+    return data.eur;
+  } catch (error) {
+    console.error("Error fetching exchange rates:", error.message);
+    return null;
+  }
+}
 
-  //selección de los botones de moneda
-  const currencyButtons = document.querySelectorAll(
-    ".section3__currencySelector__buttonContainer__buttonSelector"
+//selección de los botones de moneda
+const currencyButtons = document.querySelectorAll(
+  ".section3__currencySelector__buttonContainer__buttonSelector"
+);
+
+//Función actualizar precios
+function updatePrices(exchangeRates, baseCurrency) {
+  const prices = document.querySelectorAll(
+    ".section3__card3__containerPrice__price"
   );
+  const pricesEur = [0, 25, 60];
 
-  //función para manejar el clic en los botones
-  async function handleCurrencyChange(event) {
-    //obtener el valor del botón (eur, usd, gbp)
-    const baseCurrency = event.target.value;
-
-    try {
-      //obtener los tipos de cambio usando fetchExchangeRates
-      const exchangeRates = await fetchExchangeRates();
-      if (exchangeRates) {
-        //Recalcular y actualizar los precios en EUR, USD y GBP 
-        updatePrices(exchangeRates, baseCurrency)
-      }
-    
-    } catch (error) {
-      console.error("Error", error);
+  prices.forEach((element, index) => {
+    let newPrice;
+    if (baseCurrency === "eur") {
+      newPrice = pricesEur[index];
+    } else if (baseCurrency === "usd") {
+      newPrice = (pricesEur[index] * exchangeRates.usd).toFixed(2);
+    } else if (baseCurrency === "gbp") {
+      newPrice = (pricesEur[index] * exchangeRates.gbp).toFixed(2);
     }
-  };
-
-  //definir símbolos con monedas
-  const currencySymbols = {
-    eur: '€',
-    usd: '$',
-    gbp: '£'
-  }
-
-  //Función actualizar precios
-  function updatePrices(exchangeRates, baseCurrency) {
-    //Seleccionar todos los precios
-    const prices = document.querySelectorAll('.section3__card3__containerPrice__price');
-    const pricesEur = [0, 25, 60];
-
-    prices.forEach((element, index) => {
-      let newPrice;
-      if(baseCurrency === 'eur') {
-        newPrice = pricesEur[index];
-      } else if (baseCurrency === 'usd') {
-        newPrice = (pricesEur[index] * exchangeRates.usd).toFixed(2);
-      }else if (baseCurrency === 'gbp') {
-        newPrice = (pricesEur[index] * exchangeRates.gbp).toFixed(2);
-      }
-      element.textContent = `${currencySymbols[baseCurrency]} ${newPrice}`;
-    })
-  }
-
-  //agregar evento click a cada botón de moneda
-  currencyButtons.forEach((button) => {
-    button.addEventListener("click", handleCurrencyChange);
+    element.textContent = `${currencySymbols[baseCurrency]} ${newPrice}`;
   });
+}
+
+//función para manejar el clic en los botones
+async function handleCurrencyChangeSelector(event) {
+  const baseCurrency = event.target.value;
+
+  try {
+    const exchangeRates = await fetchExchangeRates();
+    if (exchangeRates) {
+      updatePrices(exchangeRates, baseCurrency);
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+//agregar evento click a cada botón de moneda
+currencyButtons.forEach((button) => {
+  button.addEventListener("click", handleCurrencyChangeSelector);
 });
--- scripts/formContact.js
+++ scripts/formContact.js
@@ -1,88 +1,83 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contact_form");
+const form = document.getElementById("contact_form");
 
-  //VALIDACIÓN DEL FORMULARIO SECCIÓN4
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+//VALIDACIÓN DEL FORMULARIO SECCIÓN4
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    //Validar nameContact
-    const nameContact = event.target.elements["nameContact"];
-    const nameContactError = document.getElementById("nameContactError");
+  //Validar nameContact
+  const nameContact = event.target.elements["nameContact"];
+  const nameContactError = document.getElementById("nameContactError");
 
-    const numberPattern = /\d/; //regex para comprobar que no hay números en el name
+  const numberPattern = /\d/; //regex para comprobar que no hay números en el name
 
-    if (
-      numberPattern.test(nameContact.value) || //Condición para comprobar si hay número
-      nameContact.value.length < 2 ||
-      nameContact.value.length > 100
-    ) {
-      nameContact.classList.add("invalid");
-      nameContactError.classList.add("active");
-    } else {
-      nameContact.classList.remove("invalid");
-      nameContactError.classList.remove("active");
-    }
+  if (
+    numberPattern.test(nameContact.value) || //Condición para comprobar si hay número
+    nameContact.value.length < 2 ||
+    nameContact.value.length > 100
+  ) {
+    nameContact.classList.add("invalid");
+    nameContactError.classList.add("active");
+  } else {
+    nameContact.classList.remove("invalid");
+    nameContactError.classList.remove("active");
+  }
 
-    //Validar emailContact
-    const emailContact = event.target.elements["emailContact"];
-    const emailContactError = document.getElementById("emailContactError");
+  //Validar emailContact
+  const emailContact = event.target.elements["emailContact"];
+  const emailContactError = document.getElementById("emailContactError");
 
-    const emailContactPattern =
-      /*Regex para comprobar si el email es válido*/
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailContactPattern =
+    /*Regex para comprobar si el email es válido*/
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!emailContactPattern.test(emailContact.value)) {
-      //Si el mail NO es válido, añade alertas.
-      emailContact.classList.add("invalid");
-      emailContactError.classList.add("active");
-    } else {
-      emailContact.classList.remove("invalid");
-      emailContactError.classList.remove("active");
-    }
+  if (!emailContactPattern.test(emailContact.value)) {
+    emailContact.classList.add("invalid");
+    emailContactError.classList.add("active");
+  } else {
+    emailContact.classList.remove("invalid");
+    emailContactError.classList.remove("active");
+  }
 
-    //Validar checkbox
-    const terms = event.target.elements["terms"];
-    const termsError = document.getElementById("termsError");
-    if (!terms.checked) {
-      terms.classList.add("invalidCheckbox");
-      termsError.classList.add("active");
-    } else {
-      terms.classList.remove("invalidCheckbox");
-      termsError.classList.remove("active");
-    }
+  //Validar checkbox
+  const terms = event.target.elements["terms"];
+  const termsError = document.getElementById("termsError");
+  if (!terms.checked) {
+    terms.classList.add("invalidCheckbox");
+    termsError.classList.add("active");
+  } else {
+    terms.classList.remove("invalidCheckbox");
+    termsError.classList.remove("active");
+  }
 
-    //VALIDACIÓN DEL FORMULARIO PARA SU ENVIO
-    if (
-      nameContact.value.length > 1 &&
-      emailContactPattern.test(emailContact.value) &&
-      terms.checked
-    ) {
-      //ENVIAR DATOS DE CONTACTO DEL FORMULARIO A LA TEST API
-      const apiTesting = "https://jsonplaceholder.typicode.com/posts";
+  //VALIDACIÓN DEL FORMULARIO PARA SU ENVIO
+  if (
+    nameContact.value.length > 1 &&
+    emailContactPattern.test(emailContact.value) &&
+    terms.checked
+  ) {
+    //ENVIAR DATOS DE CONTACTO DEL FORMULARIO A LA TEST API
+    const apiTesting = "https://jsonplaceholder.typicode.com/posts";
 
-      const formData = {
+    fetch(apiTesting, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
         name: nameContact.value,
-        email: emailContact.value,
-      };
-
-      fetch(apiTesting, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
+        email: emailContact.value
       })
-        .then((response) => response.json())
-        .then((json) => {
-          alert("Data sent correctly.");
-          console.log(json);
-        })
-        .catch((error) => {
-          alert("Error sending data. Please try again.");
-          console.error("Error:", error);
-        });
-    } else {
-      alert("Error sending data. Please try again.");
-    }
-  });
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        alert("Data sent correctly.");
+        console.log(json);
+      })
+      .catch((error) => {
+        alert("Error sending data. Please try again.");
+        console.error("Error:", error);
+      });
+  } else {
+    alert("Error sending data. Please try again.");
+  }
 });
