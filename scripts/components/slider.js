0543a8d243e43584b6cc68faa72ec44894a32749
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-07-14 16:23:44 +0200

    slider

---
+++ scripts/components/slider.js
@@ -0,0 +1,37 @@
+let img = [
+    "../../images/slider_img1.jpg",
+    "../../images/slider_img2.jpg",
+    "../../images/slider_img3.jpg",
+    "../../images/slider_img4.jpg",
+    "../../images/slider_img5.jpg",
+    "../../images/slider_img6.jpg"
+];
+document.img.src = img[0];
+
+let counter = 0;
+let sliderRight = document.querySelector(".right");
+let sliderLeft = document.querySelector(".left");
+let sliderImg = document.querySelector(".slider__screen img");
+
+const moveRight = () => {
+    counter++;
+console.log(counter)
+    if (counter >= img.length) {
+        counter = 0;
+    }
+
+    sliderImg.src = img[counter];
+}
+
+const moveLeft = () => {
+    counter--;
+console.log(counter)
+    if (counter < 0) {
+        counter = img.length -1;
+    }
+
+    sliderImg.src = img[counter];
+}
+
+sliderRight.addEventListener("click", moveRight);
+sliderLeft.addEventListener("click", moveLeft);
